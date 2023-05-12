22edac16c06f134a44ed6ea17f201975e27d5a4f
MariaPaulaAcero <mariaacre@unisabana.edu.co>
2023-05-12 13:46:42 -0500

    Terminado

--- src/context/MovieGlobalState.js
+++ src/context/MovieGlobalState.js
@@ -1,30 +1,51 @@
-import { createContext, useReducer, useEffect, useState } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AppReducer from "./AppReducer";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
-
-const initialState = {
-    favoriteMovie: 
-         JSON.parse(
-            localStorage.getItem('react-movie-app-favourites')
-          ),
-    
+const getFavorites = async () => {
+  const favoritesRef = collection(db, 'favorites');
+  const snapshot = await getDocs(favoritesRef);
+  return snapshot.docs.map(doc => doc.data());
 };
 
+const getWatchList = async () => {
+  const watchListsRef = collection(db, 'watchList');
+  const snapshot = await getDocs(watchListsRef);
+  return snapshot.docs.map(doc => doc.data());
+};
+const initialState = {
+    favoriteMovie: getFavoriteMovie(),
+    watchList: localStorage.getItem("watchlist")
+        ? JSON.parse(localStorage.getItem("watchlist"))
+        : [],
+};
+
+async function getFavoriteMovie() {
+  var favorites = []
+  await getFavorites().then((data) => {
+    favorites = (prev => [...prev,data]);
+  });
+
+  return favorites
+}
+
+// Llama a la función para obtener la película favorita y actualizar el estad
 
 export const MovieContext = createContext(initialState);
 
 export const MovieProvider = props => {
 
-    const [favorites, setFavorites] = useState(initialState.favoriteMovie);
-    const [watchlist, setWatchlist] = useState(initialState.watchList);   
+   const { favoriteMovie, watchList } = useReducer(AppReducer, initialState);   
+
 
 
     return (
         <MovieContext.Provider
             value={{
-                watchlist,
-                favorites,
-        
+                watchList,
+                favoriteMovie,
+
             }}
         >
             {props.children}
